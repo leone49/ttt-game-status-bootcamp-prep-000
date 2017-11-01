13c3af23bb457ea947ecbca91927f57a0d74097f
Nicholas Leone <nicholas.leone.49@gmail.com>
2017-11-01 21:45:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,10 +7,10 @@ end
 WIN_COMBINATIONS = [
   [0,1,2], #top row
     [3,4,5], #mid row
-      [6,7,8] #bot row
-        [0,3,6] #left col
-          [1,4,7] #mid col
-            [2,5,8] #right col
-              [0,4,8] #diag left-right
+      [6,7,8], #bot row
+        [0,3,6], #left col
+          [1,4,7], #mid col
+            [2,5,8], #right col
+              [0,4,8], #diag left-right
                 [2,4,6] #diag right-left
 ]
