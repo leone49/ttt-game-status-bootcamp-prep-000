eeb6a59639bedf12968315e9909943c7662cb5b0
Nicholas Leone <nicholas.leone.49@gmail.com>
2017-11-01 21:53:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,33 +5,87 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #top row
-    [3,4,5], #mid row
-      [6,7,8], #bot row
-        [0,3,6], #left col
-          [1,4,7], #mid col
-            [2,5,8], #right col
-              [0,4,8], #diag left-right
-                [2,4,6] #diag right-left
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
-
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo_array|
     first_index = combo_array[0]
-    second_index =  combo_array[1]
+    second_index = combo_array[1]
     third_index = combo_array[2]
 
-    arr = [board[first_index], board[second-index], board[third_index]]
-    all_x = arr,all? do |value|
+    arr = [board[first_index], board[second_index], board[third_index]]
+    all_x = arr.all? do |value|
       value == "X"
     end
     all_o = arr.all? do |value|
       value == "O"
     end
+
     if all_x || all_o
       return combo_array
     end
+  end
+  return false
 end
-return false
+
+def full?(board)
+  full = board.all? do |letter|
+    letter == "X" || letter == "O"
+  end
+  if full
+    true
+  else
+    false
+  end
 end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board) || won?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |combo_array|
+    first_index = combo_array[0]
+    second_index = combo_array[1]
+    third_index = combo_array[2]
+
+    arr = [board[first_index], board[second_index], board[third_index]]
+    all_x = arr.all? do |value|
+      value == "X"
+    end
+    all_o = arr.all? do |value|
+      value == "O"
+    end
+
+    if all_x
+      return "X"
+    end
+    if all_o
+      return "O"
+    end
+  end
+  return nil
+end
+
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+puts full?(board)
